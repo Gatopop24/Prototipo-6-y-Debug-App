c67d683a722dd61a953a64009b1185cf7b226412
Gatopop24 <gatopop25@gmail.com>
2026-09-08 19:43:55 -0400

    Pachinko game done

---
+++ Assets/Counter/Counter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public Text CounterText;
+    public int score;
+
+    private int count = 0;
+
+    private void Start()
+    {
+        count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        count += score;
+        CounterText.text = "Count : " + count;
+    }
+}
